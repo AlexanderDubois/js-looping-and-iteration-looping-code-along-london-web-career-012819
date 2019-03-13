9d9d992116627b53744969861e944b53491bd5f7
Alexander Dubois <sander.dubois@outlook.com>
2019-03-13 10:58:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function printBadges(employeeprintBadges) {
+  for(let i = 0; i < employeeprintBadges.length; i++) {
+    console.log(`Welcome ${employeeprintBadges[i]}! You are employee #${i + 1}.`)
+  }
+  return employeeprintBadges
+}
+
+function tailsNeverFails() {
+  let counter = 0
+  while(Math.random() >= 0.5) {
+    counter ++
+  }
+   return `You got ${counter} tails in a row!`
+}
